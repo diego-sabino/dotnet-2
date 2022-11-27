454be0086a10e94684f824bafc466f6cf77cdfa9
Diego-Sabino <dbrgamei5@gmail.com>
2022-11-27 17:28:27 -0300

    ambiente aula 1

--- AluraCS2/donet1-5.0/Program.cs
+++ AluraCS2/donet1-5.0/Program.cs
@@ -1,3 +1,4 @@
+using FilmesAPI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -7,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace donet1_5._0
+namespace FilmesApi
 {
     public class Program
     {
