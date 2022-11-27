254043a95e28ebb06c3412d3ff0e3d0b1c31c74a
Diego-Sabino <dbrgamei5@gmail.com>
2022-11-27 17:44:31 -0300

    atividade 1

--- AluraCS2/donet1-5.0/Models/Endereco.cs
+++ AluraCS2/donet1-5.0/Models/Endereco.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using FilmesAPI.Models;
 
 namespace FilmesApi.Models
 {
@@ -14,5 +15,6 @@ namespace FilmesApi.Models
         public string Logradouro { get; set; }
         public string Bairro { get; set; }
         public int Numero { get; set; }
+        public Cinema Cinema { get; set; }
     }
 }
