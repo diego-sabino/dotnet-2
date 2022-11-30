54410c130b99a4ee7f9b2475a388b3d402716b78
Diego-Sabino <dbrgamei5@gmail.com>
2022-11-30 15:10:27 -0300

    atividade 1

---
+++ AluraCS2/donet1-5.0/Controllers/GerenteController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos.Gerente;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller")]
+    public class GerenteController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public GerenteController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IActionResult AdicionaGerente(CreateGerenteDto dto)
+        {
+            Gerente gerente = _mapper.Map<Gerente>(dto);
+            _context.Gerentes.Add(gerente);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaGerentesPorId), new { Id = gerente.Id }, gerente);
+        }
+
+        public IActionResult RecuperaGerentesPorId(int id)
+        {
+            Gerente gerente = _context.Gerentes.FirstOrDefault(gerente => gerente.Id == id);
+            if (gerente != null)
+            {
+                ReadGerenteDto filmeDto = _mapper.Map<ReadGerenteDto>(gerente);
+
+                return Ok(filmeDto);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ AluraCS2/donet1-5.0/Models/Gerente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Gerente
+    {
+        [Key]
+        [Required]
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
